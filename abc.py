d0a34c815f624e11c55ffec932ed5e6998f44e2a
ok5474565 <13725981731@163.com>
2024-06-20 19:33:30 +0800

    Update abc.py

--- abc.py
+++ abc.py
@@ -1,21 +1,25 @@
-# main.py
+# abc.py
 import streamlit as st
 
-# 导入其他文件中的 run 函数
+# 导入其他文件中的 run 或 main 函数
 from app_bar2 import run as bar_chart_run
+from app_abstract import main as summary_main
 from app_worldcloud import run as wordcloud_run
 
 # 设置页面配置，只能在应用程序的最顶层调用一次
 st.set_page_config(page_title="综合文本分析工具", layout="wide")
 
 # 创建下拉列表供用户选择功能
-function_options = ["文本分析工具", "生成词云图"]
+function_options = ["文本分析工具", "新闻摘要", "生成词云图"]
 selected_option = st.sidebar.selectbox("选择功能：", options=function_options)
 
 # 根据用户选择的功能，调用相应的函数
 if selected_option == "文本分析工具":
     st.title("文本分析工具")
     bar_chart_run()
+elif selected_option == "新闻摘要":
+    st.title("新闻摘要")
+    summary_main()
 elif selected_option == "生成词云图":
     st.title("生成词云图")
     wordcloud_run()
